c4fa1e37d3276e704f489fea1d9df660cd0d09dd
Liam Fletcher <liam.fletcher@bristol.ac.uk>
2021-03-07 17:24:21 +0000

    initial pi-gen starling commit

--- stage3/01-sys-tweaks/01-run.sh
+++ stage3/01-sys-tweaks/01-run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 
+# install docker
 on_chroot - <<EOF
-curl -sSL https://get.docker.com/ | sh
-usermod -aG docker pi
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh 
+usermod -aG docker ${FIRST_USER_NAME}
 echo "installed docker"
 EOF
 
+# install Kubernetes
 on_chroot << EOF
-echo "installing k3s"
-curl -sfL https://get.k3s.io | sh -s - --docker
+cd "/home/"
+curl –fsSL https://github.com/k3s-io/k3s/releases/download/v1.20.2%2Bk3s1/k3s-airgap-images-arm64.tar  
+curl –fsSL https://github.com/k3s-io/k3s/releases/download/v1.20.2%2Bk3s1/k3s-arm64  
+curl –fsSL https://get.k3s.io/ -o k3s_install.sh 
 EOF
